7d8e608ccb80a9b7b3bc3633757165289f709286
yakubilik <yakupkeskin777@gmail.com>
2023-01-01 00:47:57 +0300

    update

--- functions.py
+++ functions.py
@@ -99,15 +99,12 @@ def predict_model(image,target_age:int,net):
     print("resized!")
 
 
-    for i in range(4):
-        try:
-            aligned_image = run_alignment(original_image)
-            print("aligned!")
-            break
-        except:
-            original_image = original_image.rotate(-90)
-            aligned_image = run_alignment(original_image)
-            break   
+    try:
+        aligned_image = run_alignment(original_image)
+        print("aligned!")
+    except:
+        original_image = original_image.rotate(-90)
+        aligned_image = run_alignment(original_image)   
 
     input_image = img_transforms(aligned_image)
     print("transform!")
